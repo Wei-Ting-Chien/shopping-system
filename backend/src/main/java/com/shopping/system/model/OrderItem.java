29b5303972ed1f19cdce8e8ae5a0c37cc7882581
Wei-Ting-Chien <weitingforwork@gmail.com>
2025-11-27 16:04:02 +0800

    Feat: Finish Product API CRUD

---
+++ backend/src/main/java/com/shopping/system/model/OrderItem.java
@@ -0,0 +1,57 @@
+package com.shopping.system.model;
+
+public class OrderItem {
+    private Integer id;
+    private Integer orderId;
+    private Integer productId;
+    
+    private String productName; 
+    private Integer price;      
+    private Integer quantity;
+    
+    //
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	public Integer getProductId() {
+		return productId;
+	}
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public Integer getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+	
+	// To-String
+	@Override
+	public String toString() {
+		return "OrderItem [id=" + id + ", orderId=" + orderId + ", productId=" + productId + ", productName="
+				+ productName + ", price=" + price + ", quantity=" + quantity + "]";
+	}
+        
+}
